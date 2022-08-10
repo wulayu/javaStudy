0eeadae3f9d3d8e8e987329a39d16677b787746a
wulayu <849519573@qq.com>
2022-08-10 15:17:01 +0800

    init

---
+++ src/hello/Hellohello.java
@@ -0,0 +1,22 @@
+package hello;
+
+public class Hellohello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+    public void run() {
+        System.out.println("la la la la");
+    }
+
+    public void runhi(){
+        hi a = new  hi();
+        a.run();
+    }
+}
+
+class hi {
+    public void run() {
+        System.out.println("hi");
+    }
+}
---
+++ src/helloWorld.java
@@ -0,0 +1,10 @@
+import hello.*;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        Hellohello hello = new Hellohello();
+        hello.run();
+        hello.main(args);
+        hello.runhi();
+    }
+}
